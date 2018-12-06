6406c08523bdba17a5c73350c42f4a2e870d932e
Aaron <azza1990uk@yahoo.com>
2018-12-06 00:26:27 +0000

    Remove bootstrap

--- src/html.js
+++ src/html.js
@@ -14,18 +14,6 @@ export default class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
-          <link
-            rel="stylesheet"
-            href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-            integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
-            crossOrigin="anonymous"
-          />
-          <link
-            rel="stylesheet"
-            href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
-            integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp"
-            crossOrigin="anonymous"
-          />
           {this.props.headComponents}
         </head>
         <body {...this.props.bodyAttributes}>
