0998f7d897544a2d36b036377826b23425162ec3
Aaron <a.albinson@teluxhd.com>
2019-01-04 15:04:06 +0000

    Add new card style

--- src/components/AaNewsItem/AaNewsItem.js
+++ src/components/AaNewsItem/AaNewsItem.js
@@ -33,6 +33,29 @@ class AaNewsItem extends React.Component {
             </a>
           </p>
         </div>
+        <div class="blog-card spring-fever">
+          <div class="title-content">
+            <h3>SPRING FEVER</h3>
+            <hr />
+            <div class="intro">
+              Yllamco laboris nisi ut aliquip ex ea commodo.
+            </div>
+          </div>
+          <div class="card-info">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim.
+          </div>
+          <div class="utility-info">
+            <ul class="utility-list">
+              <li class="comments">12</li>
+              <li class="date">03.12.2015</li>
+            </ul>
+          </div>
+
+          <div class="gradient-overlay" />
+          <div class="color-overlay" />
+        </div>
       </div>
     );
   }
