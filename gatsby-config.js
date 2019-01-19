14c37f415cf05a5a2065bcdf7d7c30fd18bd8fe2
Aaron <azza1990uk@yahoo.com>
2019-01-19 22:06:23 +0000

    Change font

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [`eb garamond`, `muli`]
+        fonts: [`vollkorn`, `muli`]
       }
     },
     {
--- src/templates/tags.js
+++ src/templates/tags.js
@@ -1,24 +1,28 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import { Link, graphql } from 'gatsby'
-import Layout from '../components/Layout'
+import React from "react";
+import Helmet from "react-helmet";
+import { Link, graphql } from "gatsby";
+import Layout from "../components/Layout";
+import AaNewsItem from "../components/AaNewsItem/AaNewsItem";
 
 class TagRoute extends React.Component {
   render() {
-    const posts = this.props.data.allMarkdownRemark.edges
+    const posts = this.props.data.allMarkdownRemark.edges;
     const postLinks = posts.map(post => (
-      <li key={post.node.fields.slug}>
-        <Link to={post.node.fields.slug}>
-          <h2 className="is-size-2">{post.node.frontmatter.title}</h2>
-        </Link>
-      </li>
-    ))
-    const tag = this.props.pageContext.tag
-    const title = this.props.data.site.siteMetadata.title
-    const totalCount = this.props.data.allMarkdownRemark.totalCount
+      <AaNewsItem
+        id={post.node.id}
+        slug={post.node.fields.slug}
+        title={post.node.frontmatter.title}
+        image={post.node.frontmatter.blogImage}
+        date={post.node.frontmatter.date}
+        excerpt={post.node.excerpt}
+      />
+    ));
+    const tag = this.props.pageContext.tag;
+    const title = this.props.data.site.siteMetadata.title;
+    const totalCount = this.props.data.allMarkdownRemark.totalCount;
     const tagHeader = `${totalCount} post${
-      totalCount === 1 ? '' : 's'
-    } tagged with “${tag}”`
+      totalCount === 1 ? "" : "s"
+    } tagged with “${tag}”`;
 
     return (
       <Layout>
@@ -28,10 +32,10 @@ class TagRoute extends React.Component {
             <div className="columns">
               <div
                 className="column is-10 is-offset-1"
-                style={{ marginBottom: '6rem' }}
+                style={{ marginBottom: "6rem" }}
               >
-                <h3 className="title is-size-4 is-bold-light">{tagHeader}</h3>
-                <ul className="taglist">{postLinks}</ul>
+                <h3 className="title">{tagHeader}</h3>
+                <div className="taglist">{postLinks}</div>
                 <p>
                   <Link to="/tags/">Browse all tags</Link>
                 </p>
@@ -40,11 +44,11 @@ class TagRoute extends React.Component {
           </div>
         </section>
       </Layout>
-    )
+    );
   }
 }
 
-export default TagRoute
+export default TagRoute;
 
 export const tagPageQuery = graphql`
   query TagPage($tag: String) {
@@ -61,14 +65,18 @@ export const tagPageQuery = graphql`
       totalCount
       edges {
         node {
+          excerpt(pruneLength: 400)
           fields {
             slug
           }
           frontmatter {
             title
+            blogImage
+            templateKey
+            date(formatString: "MMMM DD, YYYY")
           }
         }
       }
     }
   }
-`
+`;
