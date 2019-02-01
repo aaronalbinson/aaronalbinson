8964be3659d74d324fdb9ceb15a371cc80d283d3
Aaron <azza1990uk@yahoo.com>
2019-02-01 03:42:59 +0000

    Get linkedin and github from graphql

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -26,6 +26,8 @@ const TemplateWrapper = ({ children }) => (
                 sociallinks {
                   facebook
                   twitter
+                  linkedin
+                  github
                 }
               }
             }
