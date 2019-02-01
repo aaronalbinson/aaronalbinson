6a1e8958f9b81a2dc3dad516673f10c48b1148af
Aaron <azza1990uk@yahoo.com>
2019-02-01 01:12:49 +0000

    Change logo to svg

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -42,12 +42,34 @@ const TemplateWrapper = ({ children }) => (
             <div id="page-wrap">
               <div className="top">
                 <Link to="/">
-                  <img
-                    className="logo"
-                    src={logo}
-                    alt="Aaron Albinson"
-                    style={{ width: "100px" }}
-                  />
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 173.4 67.74"
+                  >
+                    <g data-name="Layer 2">
+                      <text
+                        transform="translate(0 51.36)"
+                        font-size="60"
+                        fill="#fff"
+                        font-family="Muli-SemiBold,Muli"
+                        font-weight="700"
+                        data-name="Layer 1"
+                      >
+                        Aa
+                        <tspan
+                          x="81"
+                          y="0"
+                          letter-spacing="-.01em"
+                          fill="#d6d9d0"
+                        >
+                          r
+                        </tspan>
+                        <tspan x="102.96" y="0" fill="#d6d9d0">
+                          on
+                        </tspan>
+                      </text>
+                    </g>
+                  </svg>
                 </Link>
               </div>
               {children}
