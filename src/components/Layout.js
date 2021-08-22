a086925997a20a4b36f219cb9c457a4d32de6b20
aaronalbinson <azza1990uk@yahoo.com>
2021-08-23 00:14:52 +0100

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -33,7 +33,7 @@ class Layout extends React.Component {
                 <div className="top-inner container">
                   <Link to="/">
                     <span className="siteTitle">Aaron Albinson</span>
-                    <meta name="description" content="Website developer based in Manchester UK.">
+                    <meta name="description" content="Website developer based in Manchester UK."/>
                       <svg width="60" height="44" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M50.277 37.71H39.36L37.387 44h-6.68l10.977-30.04h6.289L59.047 44H52.27l-1.993-6.29zm-1.425-4.472l-3.223-10.195-.84-3.066-.86 3.066-3.183 10.195h8.106z" fill="#A28845" /><path class="logo-a" d="M.24 44v-4.14c1.12 0 2.02-.32 2.7-.96.68-.64 1.24-1.48 1.68-2.52.48-1.04.94-2.16 1.38-3.36L17.7.92h4.2l12.78 32.7c.24.6.56 1.52.96 2.76.4 1.2.64 2.26.72 3.18.68-.08 1.32-.12 1.92-.12.64-.04 1.16-.06 1.56-.06V44H23.7v-3.96c1.64 0 2.56-.5 2.76-1.5.2-1.04.02-2.3-.54-3.78l-1.2-3.3-13.02.24-.96 2.82-.9 2.34c-.36 1-.7 1.84-1.02 2.52.64-.08 1.4-.12 2.28-.12.92-.04 1.58-.06 1.98-.06V44H.24zM13.2 27.2h10.02l-2.76-7.5c-.52-1.44-.98-2.88-1.38-4.32-.4-1.48-.7-2.78-.9-3.9h-.06l-.72 3.06c-.32 1.36-.82 2.98-1.5 4.86l-2.7 7.8z" fill="#EEE" /></svg>
                   </Link>
                     <Navbar />
