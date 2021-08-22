d08af3586117df0660ba2f50aa7c43f8111fa4ab
aaronalbinson <azza1990uk@yahoo.com>
2021-08-23 00:22:04 +0100

    Use google fonts v2

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,9 +60,16 @@ module.exports = {
       }
     },
     {
-      resolve: `gatsby-plugin-google-fonts`,
+      resolve: `gatsby-plugin-google-fonts-v2`,
       options: {
-        fonts: [`roboto`, `lora`]
+        fonts: [
+          {
+            family: 'Roboto'
+          },
+          {
+            family: 'Lora'
+          }
+        ]
       }
     },
     {
